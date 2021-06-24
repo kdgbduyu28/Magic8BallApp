9f5aad9843ac90d553627287fa1cbeea1213aefe
Timothy Borja <kdgbduyu28@gmail.com>
2021-06-24 23:46:38 +0800

    initial release

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,42 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+          home: Scaffold(
+        backgroundColor: Colors.blue,
+        appBar: AppBar(
+          backgroundColor: Colors.blue[900],
+          title: Text("Ask me Anything"),
+          centerTitle: true,
+        ),
+        body: MagicBall(),
+      )),
+    );
+
+class MagicBall extends StatefulWidget {
+  const MagicBall({Key? key}) : super(key: key);
+
+  @override
+  _MagicBallState createState() => _MagicBallState();
+}
+
+class _MagicBallState extends State<MagicBall> {
+  int ballNum = 1;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: TextButton(
+          onPressed: () {
+            setState(() {
+              ballNum = Random().nextInt(5) + 1;
+            });
+          },
+          child: Image.asset("images/ball$ballNum.png"),
+        ),
       ),
     );
+  }
+}
